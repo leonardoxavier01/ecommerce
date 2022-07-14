65c9036fa1ef01744429265193ace381f6d13a21
unknown <leonardoxavier092@gmail.com>
2022-07-14 01:04:39 -0300

    feat: type e props color button

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -3,11 +3,16 @@ import { ButtonStyled } from "./styles";
 
 type ButtonProps = {
   children: ReactNode;
+  color: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
 const Button = (props: ButtonProps) => {
-  return <ButtonStyled onClick={props.onClick}>{props.children}</ButtonStyled>;
+  return (
+    <ButtonStyled color={props.color} onClick={props.onClick}>
+      {props.children}
+    </ButtonStyled>
+  );
 };
 
 export default Button;
--- components/Button/styles.ts
+++ components/Button/styles.ts
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 
-export const ButtonStyled = styled.button`
+interface IProps {
+  color: string;
+}
+
+export const ButtonStyled = styled.button<IProps>`
   width: 226px;
   height: 43px;
   border-radius: 10px;
   padding: 5px;
   text-align: center;
-  background-color: #5d2d8c;
+  background-color: ${(props) => props.color || "#5d2d8c"};
   color: white;
   font-size: 16px;
   font-weight: 700;
