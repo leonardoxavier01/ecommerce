f92bf1ea42d39b6e48f4fb12312e7fddcadda265
unknown <leonardoxavier092@gmail.com>
2022-07-16 19:05:21 -0300

    fix Width containerPage

--- components/ContainerPage/styles.ts
+++ components/ContainerPage/styles.ts
@@ -10,4 +10,15 @@ export const WrapperPage = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  @media (min-width: 300px) {
+    width: 100%;
+  }
+
+  @media (min-width: 600px) {
+    width: 95%;
+  }
+
+  @media (min-width: 900px) {
+    max-width: 1200px;
+  }
 `;
