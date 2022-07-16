27ba0e2cbc380a77e8f00b0fa9c7ed0169f74126
unknown <leonardoxavier092@gmail.com>
2022-07-16 18:33:49 -0300

    feat: component containerPage

---
+++ components/ContainerPage/index.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+import { Container, WrapperPage } from "./styles";
+
+interface IContainerPageProps {
+  children: JSX.Element | JSX.Element[] | string | string[];
+}
+
+const ContainerPage = ({ children }: IContainerPageProps) => {
+  return (
+    <Container>
+      <WrapperPage>{children}</WrapperPage>
+    </Container>
+  );
+};
+
+export default ContainerPage;
---
+++ components/ContainerPage/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+`;
+export const WrapperPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
