4d244e5db5261bfca75de08bd8e60279d9cfd918
unknown <leonardoxavier092@gmail.com>
2022-07-16 20:50:46 -0300

    feat: props cardCircle in Id

--- components/CardCircle/index.tsx
+++ components/CardCircle/index.tsx
@@ -3,11 +3,12 @@ import Image, { StaticImageData } from "next/image";
 
 interface ICardCircle {
   image: StaticImageData | string;
+  categoryName: string;
 }
 
-const CardCircle = ({ image}: ICardCircle) => {
+const CardCircle = ({ image, categoryName }: ICardCircle) => {
   return (
-    <Container>
+    <Container id={categoryName}>
       <BoxImage>
         <Image src={image} width={200} height={200} alt="marvel" />
       </BoxImage>
