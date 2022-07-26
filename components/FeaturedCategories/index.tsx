30fc902025d49abc2cf3ab12e2d1c3bb81ee6b49
unknown <leonardoxavier092@gmail.com>
2022-07-25 23:49:00 -0300

    feat: altercategory

--- components/FeaturedCategories/index.tsx
+++ components/FeaturedCategories/index.tsx
@@ -1,7 +1,7 @@
 import { BoxButtonImage, Container } from "./styles";
 import Image from "next/image";
-import canecas from "../../assets/images/canecas.png";
-import camisetas from "../../assets/images/camisetas.png";
+import category1 from "../../assets/images/starwars-c.png";
+import category2 from "../../assets/images/dccomics-c.png";
 import Button from "../Button";
 import Link from "next/link";
 
@@ -10,21 +10,21 @@ const FeaturedCategories = () => {
     <Container>
       <BoxButtonImage color="linear-gradient(90deg, rgba(93,45,140,1) 0%, rgba(24,67,77,1) 100%)">
         <div>
-          <Image src={camisetas} width={421} height={267} alt="camisetas" />
+          <Image src={category1} width={421} height={267} alt="camisetas" />
         </div>
         <Link href="/categories/cl5mt91bo00230myy8a4gvele" passHref>
           <a>
-            <Button>CAMISETAS</Button>
+            <Button>Ver mais</Button>
           </a>
         </Link>
       </BoxButtonImage>
       <BoxButtonImage color="linear-gradient(90deg, rgba(171,145,168,1) 0%, rgba(93,45,140,1) 100%)">
         <div>
-          <Image src={canecas} width={421} height={267} alt="canecas" />
+          <Image src={category2} width={421} height={267} alt="canecas" />
         </div>
         <Link href="/categories/cl5mt5pm900060myytuifzazm" passHref>
           <a>
-            <Button>CANECAS</Button>
+            <Button>Ver mais</Button>
           </a>
         </Link>
       </BoxButtonImage>
