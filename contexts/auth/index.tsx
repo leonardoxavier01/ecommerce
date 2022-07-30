037c1ce7ffab312671aff02a5a95363c797c2760
unknown <leonardoxavier092@gmail.com>
2022-07-30 15:33:05 -0300

    feat: context auth

---
+++ contexts/auth/index.tsx
@@ -0,0 +1,100 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import Router from "next/router";
+import ContainerPage from "../../components/ContainerPage";
+import Input from "../../components/Input";
+import { ContainerLogin, StyledForm } from "./styles";
+
+interface IAdminAuthProvider {
+  children: React.ReactNode;
+}
+
+const AuthContext = createContext({});
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log(email);
+    console.log(password);
+
+    const response = await fetch("http://localhost:5000/admin/auth", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    const { auth, token } = await response.json();
+
+    if (auth) {
+      localStorage.setItem("token", token);
+
+      Router.reload();
+    }
+  };
+
+  return (
+    <ContainerLogin>
+      <StyledForm onSubmit={onSubmit}>
+        <h1>Faça seu Login</h1>
+        <Input
+          placeholder="Email"
+          type="email"
+          id="email"
+          onChange={(e: any) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          id="password"
+          onChange={(e: any) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </StyledForm>
+    </ContainerLogin>
+  );
+};
+
+export const AdminAuthProvider = ({ children }: IAdminAuthProvider) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const token = localStorage.getItem("token");
+
+      if (token) {
+        const response = await fetch("http://localhost:5000/admin/me", {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        const result = await response.json();
+
+        if (result.auth) {
+          setIsAuthenticated(true);
+        }
+      }
+    })();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{}}>
+      <ContainerPage color={!isAuthenticated ? "#5d2d8c" : ""}>
+        {isAuthenticated ? children : <Login />}
+      </ContainerPage>
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
---
+++ contexts/auth/styles.ts
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const ContainerLogin = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 90vh;
+`;
+
+export const StyledForm = styled.form`
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  width: 25rem;
+  height: 27rem;
+  box-shadow: -1px 7px 15px 1px rgba(0, 0, 0, 0.64);
+  h1 {
+    color: #282d2e;
+    font-size: 30px;
+    margin-bottom: 1rem;
+  }
+  button {
+    background-color: #5d2d8c;
+    border-radius: 0.4rem;
+    margin-top: 2rem;
+    width: 22rem;
+    height: 2.7rem;
+    box-shadow: -1px 7px 15px -6px rgba(0, 0, 0, 0.64);
+    cursor: pointer;
+    border: transparent;
+    color: white;
+  }
+
+  div {
+    box-shadow: -5px 5px 15px -10px rgba(0, 0, 0, 0.64);
+    border: 1px solid rgba(0, 0, 0, 0.2);
+  }
+`;
