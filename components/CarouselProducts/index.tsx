c16faffe808aa1963fa9117965b8ec490b8d4dbe
unknown <leonardoxavier092@gmail.com>
2022-07-29 22:39:40 -0300

    feat: carousel products api

---
+++ components/CarouselProducts/index.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { ProductProps } from "../../types/interfaces";
+import Carousel from "../Carousel";
+import ProductCard from "../ProductCard";
+
+interface ICarouselProducts {
+  categoryName: string;
+  categoryId: string | undefined;
+}
+
+const CarouselProducts = ({ categoryName, categoryId }: ICarouselProducts) => {
+  const [products, setProducts] = useState<ProductProps[]>([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/categories/${categoryId}/products`)
+      .then((resp) => resp.json())
+      .then(setProducts);
+  }, [categoryId]);
+
+  return (
+    <Carousel color="rgb(93, 45, 140, 0.2)" categoryName={categoryName}>
+      {products.map((product) => (
+        <div key={product.id}>
+          <Link
+            href={{
+              pathname: "/productsTest/[slug]",
+              query: { slug: product.slug },
+            }}
+          >
+            <a>
+              <ProductCard
+                slug={product.slug}
+                name={product.name}
+                price={product.price}
+                priceWithDiscount={product.priceWithDiscount}
+              />
+            </a>
+          </Link>
+        </div>
+      ))}
+    </Carousel>
+  );
+};
+
+export default CarouselProducts;
