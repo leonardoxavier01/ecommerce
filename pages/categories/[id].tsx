f416b389f0a3b42d09a9f31442583322bf95ee3c
unknown <leonardoxavier092@gmail.com>
2022-07-31 23:40:24 -0300

    feat: styled categories page

--- pages/categories/[id].tsx
+++ pages/categories/[id].tsx
@@ -1,43 +1,46 @@
 import type { NextPage, GetServerSideProps } from "next";
-import { CategoryPageProps } from "../../types/interfaces";
+import { CategoryPageProps, ProductProps } from "../../types/interfaces";
 import product_services from "../../services/product_services";
 import Link from "next/link";
-import Price from "../../components/Price";
+import GridCards from "../../components/GridCards";
+import ProductCard from "../../components/ProductCard";
+import ContainerPage from "../../components/ContainerPage";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { query } = context
-  const id = query.id?.toString()
+  const { query } = context;
+  const id = query.id?.toString();
 
-  const category = await product_services.find(id)
+  const category = await product_services.find(id);
 
   return { props: { category } };
 };
 
 const CategoryPage: NextPage<CategoryPageProps> = ({ category }) => {
+  console.log(category.name);
   return (
-    <div>
-      <h1>{category.name}</h1>
-      <div>
-        {category.products.map((product: any) => (
+    <ContainerPage>
+      <GridCards categoryName={category.name}>
+        {category.products.map((product: ProductProps) => (
           <div key={product.id}>
-            <h2>
-              <Link
-                href={{
-                  pathname: "/products/[slug]",
-                  query: { slug: product.slug },
-                }}
-              >
-                {product.name}
-              </Link>
-            </h2>
-            <Price
-              price={product.price}
-              priceWithDiscount={product.priceWithDiscount}
-            />
+            <Link
+              href={{
+                pathname: "/products/[slug]",
+                query: { slug: product.slug },
+              }}
+            >
+              <a>
+                <ProductCard
+                  name={product.name}
+                  price={product.price}
+                  priceWithDiscount={product.priceWithDiscount}
+                  slug={product.slug}
+                />
+              </a>
+            </Link>
           </div>
         ))}
-      </div>
-    </div>
+      </GridCards>
+    </ContainerPage>
   );
 };
 
