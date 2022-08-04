3af819f3ae20e80b3a87f82454611fc5304914a0
unknown <leonardoxavier092@gmail.com>
2022-08-04 01:11:33 -0300

    feat: image undefined

--- components/CarouselProducts/index.tsx
+++ components/CarouselProducts/index.tsx
@@ -32,7 +32,11 @@ const CarouselProducts = ({ categoryName, categoryId }: ICarouselProducts) => {
               <ProductCard
                 slug={product.slug}
                 name={product.name}
-                image={`http://localhost:5000/images/${product.image}`}
+                image={
+                  product.image
+                    ? `http://localhost:5000/images/${product.image}`
+                    : ""
+                }
                 price={product.price}
                 priceWithDiscount={product.priceWithDiscount}
               />
--- components/ProductCard/index.tsx
+++ components/ProductCard/index.tsx
@@ -2,15 +2,28 @@ import { Container, BoxImage, Text, PriceWrapper } from "./styles";
 import { ProductProps } from "../../types/interfaces";
 import Button from "../Button";
 import Price from "../Price";
+import imageUndefined from "../../assets/images/image-undefined.jpg";
+import Image from "next/image";
 
 const ProductCard = (props: ProductProps) => {
   return (
     <Container>
       <BoxImage>
-        <img src={props.image} alt={`imagem do produto ${props.name}`} />
+        {props.image ? (
+          <img src={props.image} alt={`imagem do produto ${props.name}`} />
+        ) : (
+          <Image
+            src={imageUndefined}
+            alt={`produto sem imagem`}
+            width={240}
+            height={240}
+          />
+        )}
       </BoxImage>
       <Text>
-        <span>{props.name} - {props.headline}</span>
+        <span>
+          {props.name} - {props.headline}
+        </span>
       </Text>
       <PriceWrapper>
         <Price
