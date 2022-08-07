91c8275103b35b3a52ecbef85cec1926dea91f01
unknown <leonardoxavier092@gmail.com>
2022-08-07 02:09:03 -0300

    style: border buttonOpenModal

--- components/ButtonOpenModal/styles.ts
+++ components/ButtonOpenModal/styles.ts
@@ -7,9 +7,9 @@ interface IButtonStyled {
 
 export const ButtonStyled = styled.button<IButtonStyled>`
   background-color: ${(props) => props.backgroundColor};
-  border: transparent;
+  border: 1px solid gray;
   color: ${(props) => (props.colorFont ? props.colorFont : "black")};
-  min-width: 7.1rem;
+  min-width: 8rem;
   height: 1.4rem;
   border-radius: 1rem;
   cursor: pointer;
