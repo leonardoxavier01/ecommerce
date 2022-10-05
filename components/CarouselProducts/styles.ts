fb8a9ad91166293e81317174753072dd15bf9adc
Leonardo -Software Developer <leonardoxavier092@gmail.com>
2022-10-04 23:30:36 -0300

    styles: underline link

--- components/CarouselProducts/styles.ts
+++ components/CarouselProducts/styles.ts
@@ -1,13 +1,19 @@
 import styled from "styled-components";
 
 export const CategoryNameStyled = styled.a`
-  margin-left: 15px;
-  font-weight: 700;
+  margin: 0 0 5px 15px;
+  font-weight: 600;
   font-size: 25px;
   color: #181b1c;
   width: 100%;
   cursor: pointer;
+
   @media (max-width: 400px) {
     font-size: 19px;
+  }
+
+  :hover {
+    text-decoration: underline;
+    color: #2f3841;
   }
 `;
