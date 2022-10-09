b41adb006428153653e265ebfe1b30ae248bd5f6
Leonardo -Software Developer <leonardoxavier092@gmail.com>
2022-10-09 09:58:08 -0300

    feat: stylization/buy confirmed

--- pages/checkout/success.tsx
+++ pages/checkout/success.tsx
@@ -1,7 +1,19 @@
-import ContainerPage from "../../components/ContainerPage";
+import {
+  BuyConfirmed,
+  ContainerSucces,
+} from "../../styles/pages/CheckoutSucces";
+import { IoIosCheckmarkCircleOutline } from "react-icons/io";
 
 const Success = () => {
-  return <ContainerPage>Success</ContainerPage>;
+  return (
+    <ContainerSucces>
+      <BuyConfirmed>
+        <IoIosCheckmarkCircleOutline size={100} color={'#1DD54D'}/>
+        <h1>Pagamento realizado com sucesso</h1>
+        <h2>Estamos processando seu pedido</h2>
+      </BuyConfirmed>
+    </ContainerSucces>
+  );
 };
 
 export default Success;
